6a076416c668e9b0c2b75fc42ab424af2ec4d51c
Anooaa <leegaeun250@gmail.com>
2026-07-24 10:03:08 +0700

    update 14

--- src/config/api.js
+++ src/config/api.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'dunax-web-management-production.up.railway.app'
+export const BASE_URL = 'https://dunax-web-management-production.up.railway.app'
 export const AUTH_URL = 'https://dunax-web-penjualan.up.railway.app';
